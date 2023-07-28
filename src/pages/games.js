ddc1c5c645de692761707e7b82ef2309e4d4b218
briaguya <briaguya@alice>
2023-07-28 12:48:16 -0400

    trying to get somewhere

--- src/pages/games.js
+++ src/pages/games.js
@@ -3,11 +3,11 @@ import GamesContent from "../content/games.mdx"
 import Navigation from "../components/navigation"
 
 const gamesNavClasses = {
-    container: "bg-[#002233] md:h-screen grid grid-cols-2 pt-8",
+    container: "bg-[#002233] h-20 md:h-screen grid lg:grid-cols-3 pt-2 md:pt-8",
     byteswap: {
-        container: "max-w-xs justify-self-end",
-        logo: "max-w-xs absolute z-10 fill-[#007064] dark:fill-[#1ea699]",
-        text: "max-w-xs absolute z-0 fill-[#002233] dark:fill-white"
+        container: "pl-4 md:pl-0 pb-2 h-14 md:h-auto w-xs md:col-span-2 md:col-start-3 xl:col-start-2",
+        logo: "h-14 md:h-32 absolute z-10 fill-[#007064] dark:fill-[#1ea699]",
+        text: "h-14 md:h-32 absolute z-0 fill-[#002233] dark:fill-white md:max-xl:invisible"
     }
 }
 
