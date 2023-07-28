d713fdd0899a622f68cbe5c7d864d8adb8ee9c63
briaguya <briaguya@alice>
2023-07-28 14:05:31 -0400

    just the logo looks good

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,11 +1,13 @@
 import * as React from "react"
 import ByteswapLogo from "../svgs/byteswaplogo.svg";
 import ByteswapText from "../svgs/byteswaptext.svg";
+import ByteswapJustLogo from "../svgs/byteswap_justlogo.svg";
 
 const Navigation = ({navClasses}) => {
   return (
     <div className={navClasses.container}>
         <div className={navClasses.byteswap.container}>
+            <ByteswapJustLogo id="Byteswap Just Logo" aria-label="Byteswap Logo" className={navClasses.byteswap.justlogo} />
             <ByteswapLogo id="Byteswap Logo" aria-label="Byteswap Logo" className={navClasses.byteswap.logo} />
             <ByteswapText id="Byteswap Labs" aria-label="Byteswap Labs" className={navClasses.byteswap.text} />
         </div>
--- src/pages/games.js
+++ src/pages/games.js
@@ -3,11 +3,12 @@ import GamesContent from "../content/games.mdx"
 import Navigation from "../components/navigation"
 
 const gamesNavClasses = {
-    container: "bg-[#002233] h-20 md:h-screen grid lg:grid-cols-3 pt-2 md:pt-8",
+    container: "bg-[#002233] h-20 md:h-screen pt-2 md:pt-8 grid",
     byteswap: {
-        container: "pl-4 md:pl-0 pb-2 h-14 md:h-auto w-xs md:col-span-2 md:col-start-3 xl:col-start-2",
-        logo: "h-14 md:h-32 absolute z-10 fill-[#007064] dark:fill-[#1ea699]",
-        text: "h-14 md:h-32 absolute z-0 fill-[#002233] dark:fill-white md:max-xl:invisible"
+        container: "px-4 md:pl-0 pb-2 h-14 md:h-auto w-fit md:justify-self-end",
+        justlogo: "h-14 w-14 md:w-auto md:h-32 fill-[#007064] dark:fill-[#1ea699]",
+        logo: "h-14 md:h-32 absolute z-10 fill-[#007064] dark:fill-[#1ea699] collapse",
+        text: "h-14 md:h-32 absolute z-0 fill-[#002233] dark:fill-white collapse"
     }
 }
 
