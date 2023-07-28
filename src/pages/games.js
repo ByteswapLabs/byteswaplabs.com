ac978a59bf1787188a975122d1332df392887681
briaguya <briaguya@alice>
2023-07-28 16:12:05 -0400

    feeling good about the logo everywhere

--- src/pages/games.js
+++ src/pages/games.js
@@ -5,7 +5,7 @@ import Navigation from "../components/navigation"
 const gamesNavClasses = {
     container: "bg-[#002233] h-20 md:h-screen pt-2 md:pt-8 grid",
     byteswap: {
-        container: "px-4 md:pl-0 pb-2 h-14 md:h-auto w-fit lg:w-[280px] md:justify-self-end",
+        container: "px-4 md:pl-0 pb-2 h-14 md:h-auto w-fit md:justify-self-end",
         justlogo: "h-14 w-14 md:w-auto md:h-32 hidden md:max-lg:block",
         logoandtext: "h-14 md:h-32 md:w-auto md:max-lg:hidden",
     }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,19 @@
 import * as React from "react"
-import ByteswapLogo from "../svgs/byteswaplogo.svg";
-import ByteswapText from "../svgs/byteswaptext.svg";
-
 import { MdEmail } from 'react-icons/md';
 import { AiFillGithub, AiOutlineTwitter } from 'react-icons/ai';
 import Navigation from "../components/navigation";
 
-// const buttonClasses = " ";0b1a68
-const linkClasses = "bg-[#007064] dark:bg-[#007064]";
-const iconContainerClasses = "flex mx-6 my-4 w-10 h-10 rounded-lg bg-[#007064] dark:bg-[#007064]";
-const iconClasses = "mx-auto place-self-center fill-white h-7 w-7";
-
 const indexNavClasses = {
-  container: "md:col-start-2 self-center",
+  container: "md:max-lg:col-span-3 md:max-lg:col-start-2 lg:col-start-2 self-center",
   byteswap: {
-    container: "h-48",
-    logoandtext: "p-6 md:p-0 absolute w-full md:w-1/3 h-48",
-    logo: "fill-[#007064] dark:fill-[#1ea699]",
-    text: "fill-[#002233] dark:fill-white",
+    logoandtext: "p-6 md:p-0 w-full",
     justlogo: "hidden",
   }
 }
 
 const IndexPage = () => {
   return (
-      <main className="bg-[#ffeed0] dark:bg-[#002233] w-screen h-screen grid md:grid-cols-3">
+      <main className="bg-[#ffeed0] dark:bg-[#002233] w-screen h-screen grid md:max-lg:grid-cols-5 lg:grid-cols-3">
         <Navigation navClasses={indexNavClasses} />
       </main>
   )
