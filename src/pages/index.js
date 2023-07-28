4c058881982e001549644b240379f74f27bb00ae
briaguya <briaguya@alice>
2023-07-27 23:31:14 -0400

    color fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import ByteswapText from "../svgs/byteswaptext.svg";
 import { MdEmail } from 'react-icons/md';
 import { AiFillGithub, AiOutlineTwitter } from 'react-icons/ai';
 
-// const buttonClasses = " ";
+// const buttonClasses = " ";0b1a68
 const linkClasses = "bg-[#007064] dark:bg-[#007064]";
 const iconContainerClasses = "flex mx-6 my-4 w-10 h-10 rounded-lg bg-[#007064] dark:bg-[#007064]";
 const iconClasses = "mx-auto place-self-center fill-white h-7 w-7";
@@ -26,7 +26,7 @@ const IndexPage = () => {
         </div>
         <div className="md:col-start-2 row-start-2">
           <ByteswapLogo id="Byteswap Logo" aria-label="Byteswap Logo" className="p-6 md:p-0 absolute z-10 fill-[#007064] dark:fill-[#1ea699] w-full md:w-1/3 h-fit " />
-          <ByteswapText id="Byteswap Labs" aria-label="Byteswap Labs" className="p-6 md:p-0 absolute z-0 fill-[#007064] dark:fill-white w-full md:w-1/3 h-fit" />
+          <ByteswapText id="Byteswap Labs" aria-label="Byteswap Labs" className="p-6 md:p-0 absolute z-0 fill-[#002233] dark:fill-white w-full md:w-1/3 h-fit" />
         </div>
       </main>
   )
