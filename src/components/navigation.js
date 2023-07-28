fdcb527b69993ed862736ce9dae3fb4229f78287
briaguya <briaguya@alice>
2023-07-28 17:33:52 -0400

    use link

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,14 +1,15 @@
 import * as React from "react"
+import { Link } from "gatsby"
 import Byteswap from "../svgs/byteswap_color.svg";
 import ByteswapLogo from "../svgs/byteswaplogo_color.svg";
 
 const Navigation = ({navClasses}) => {
   return (
     <div className={navClasses.container}>
-        <div className={navClasses.byteswap.container}>
-            <ByteswapLogo id="Byteswap Logo" aria-label="Byteswap Logo" className={navClasses.byteswap.justlogo} />
-            <Byteswap id="Byteswap Labs" aria-label="Byteswap Labs" className={navClasses.byteswap.logoandtext} />
-        </div>
+      <Link to="/" className={navClasses.byteswap.container}>
+          <ByteswapLogo id="Byteswap Logo" aria-label="Byteswap Logo" className={navClasses.byteswap.justlogo} />
+          <Byteswap id="Byteswap Labs" aria-label="Byteswap Labs" className={navClasses.byteswap.logoandtext} />
+      </Link>
     </div>
   )
 }
