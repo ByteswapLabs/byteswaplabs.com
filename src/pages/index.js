ee88303992dd766aa3e38a59012ca20df4027abe
briaguya <briaguya@alice>
2023-07-29 03:16:11 -0400

    no dark mode on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Navigation from "../components/navigation";
 
 const IndexPage = () => {
   return (
-      <main className="bg-[#ffeed0] dark:bg-[#002233] w-screen h-screen grid grid-cols-1">
+      <main className="bg-[#002233] w-screen h-screen grid grid-cols-1">
         <Navigation index />
       </main>
   )
