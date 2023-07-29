7eed851e237677acf3c2e42e4af1ad4d2a2cf80e
briaguya <briaguya@alice>
2023-07-29 03:14:26 -0400

    mdx images and stuff seem to be working now

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,40 +6,63 @@ module.exports = {
     title: `Byteswap Labs`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `content`,
-      path: `${__dirname}/src/content`,
-    },
-  }, {
-    resolve: 'gatsby-plugin-react-svg',
-    options: {
-      "path": "./src/svgs/"
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-catch-links",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }, {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content`,
+        path: `${__dirname}/src/content`,
+      },
+    }, {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        "path": "./src/svgs/"
+      }
+    }, {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    }, {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        icon: `src/svgs/favicon2.svg`,
+      },
+    }, {
+      resolve: 'gatsby-plugin-html-attributes',
+      options: {
+        lang: 'en'
+      }
+    }, {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          `gatsby-remark-copy-linked-files`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 640,
+            },
+          }
+        ]
+      }
     }
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }, {
-    resolve: `gatsby-plugin-manifest`,
-    options: {
-      icon: `src/svgs/favicon2.svg`,
-    },
-  }, {
-    resolve: 'gatsby-plugin-html-attributes',
-    options: {
-      lang: 'en'
-    }
-  }]
+  ]
 };
