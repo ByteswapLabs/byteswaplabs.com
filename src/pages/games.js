98eb1746aebc214690d2ace3a35db81728d2e54d
briaguya <briaguya@alice>
2023-07-29 01:56:01 -0400

    pages and stuff

--- src/pages/games.js
+++ src/pages/games.js
@@ -1,48 +1,9 @@
 import * as React from "react"
+import ContentPage from "../components/contentpage"
 import GamesContent from "../content/games.mdx"
-import Navigation from "../components/navigation"
-
-const gamesNavClasses = {
-    container: "bg-[#002233] h-20 md:h-full md:min-h-screen pt-2 md:pt-8 grid",
-    linkscontainer: "px-4 md:pl-0 pb-2 h-14 md:h-auto w-screen md:w-fit md:justify-self-end md:fixed flex md:grid ",
-    byteswap: {
-        link: "justify-self-end",
-        justlogo: "h-14 w-14 md:w-auto md:h-32 hidden md:max-lg:block",
-        logoandtext: "h-14 md:h-32 md:w-auto md:max-lg:hidden",
-    },
-    pagelinkscontainer: "flex ml-8 mr-4 md:flex-col md:mt-8 grow md:grow-0",
-    page: {
-      link: "mx-1 justify-self-end grid grid-cols-2 md:mb-6 grow md:grow-0",
-      icon: "h-8 w-8 md:h-10 md:w-10 self-center justify-self-end",
-      text: "text-white max-sm:hidden text-2xl pl-1 md:pl-2 font-bold self-center justify-self-start"
-    },
-    games: {
-      link: "mt-2",
-      icon: "fill-[#9e6ee5] ",
-      text: "pt-2 md:pt-3"
-    },
-    about: {
-      link: "mt-4",
-      icon: "fill-[#e56e79] px-2",
-      text: ""
-    },
-    socials: {
-      link: "mt-4",
-      icon: "fill-[#b5e56e] px-1",
-      text: ""
-    }
-}
-
-const gamesContentContainerClasses = "bg-[#ffeed0] dark:bg-[#313338] dark:text-white md:col-start-2 md:col-span-2 md:h-full max-md:min-h-full"
-// const gamesContentContainerClasses = "bg-[#2A0033] md:col-start-2 md:col-span-2"
 
 const GamesPage = () => (
-  <main className="w-screen h-screen md:grid md:grid-cols-3">
-    <Navigation navClasses={gamesNavClasses} />
-    <div className={gamesContentContainerClasses}>
-      <div className="prose dark:prose-invert px-4 md:px-8 pt-2 md:pt-8"><GamesContent /></div>
-    </div>
-  </main>
+  <ContentPage><GamesContent /></ContentPage>
 )
 
 export default GamesPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,42 +1,10 @@
 import * as React from "react";
 import Navigation from "../components/navigation";
 
-const indexNavClasses = {
-  container: "self-center",
-  linkscontainer: "grid grid-cols-1",
-  byteswap: {
-    logoandtext: "p-6 md:p-0 w-full md:w-2/3 lg:w-1/2 xl:w-1/3 2xl:w-1/4 md:mx-auto",
-    justlogo: "hidden",
-  },
-  pagelinkscontainer: "justify-self-center grid grid-cols-1 md:flex md:justify-center md:w-full md:mt-6",
-  page: {
-    link: "my-4 md:mx-8",
-    icon: "h-16 w-16 inline",
-    text: "pl-3 align-middle text-white text-4xl font-bold inline md:pl-0"
-  },
-  games: {
-    link: "mt-2",
-    icon: "fill-[#9e6ee5]",
-    text: "pt-5 md:pl-2"
-  },
-  about: {
-    link: "",
-    icon: "fill-[#e56e79] px-3",
-    text: ""
-  },
-  socials: {
-    link: "",
-    icon: "fill-[#b5e56e] px-2",
-    text: ""
-  }
-}
-
-
-
 const IndexPage = () => {
   return (
       <main className="bg-[#ffeed0] dark:bg-[#002233] w-screen h-screen grid grid-cols-1">
-        <Navigation navClasses={indexNavClasses} />
+        <Navigation index />
       </main>
   )
 }
