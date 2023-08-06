6a624ed97c0223448874196c758bd0572d371fd3
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2023-08-06 16:53:53 -0400

    fixes from discussion (#29)
    * update nvmrc to use same version as vercel, move some pngs to image dir
    
    * wider content

--- src/components/contentpage.js
+++ src/components/contentpage.js
@@ -4,7 +4,7 @@ import Navigation from "../components/navigation"
 const contentClasses = {
   page: "w-screen h-screen md:grid md:grid-cols-3",
   container: "bg-[#ffeed0] dark:bg-[#313338] dark:text-white md:col-start-2 md:col-span-2 md:h-full max-md:min-h-full",
-  prose: "prose dark:prose-invert px-4 md:px-8 pt-2 md:pt-8"
+  prose: "prose dark:prose-invert px-4 md:px-8 pt-2 md:pt-8 max-w-3xl"
 };
 
 const ContentPage = props => (
