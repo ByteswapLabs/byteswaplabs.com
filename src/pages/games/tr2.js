a42303d7c12599ff35628ef62187229561b52381
Kenix3 <kenixwhisperwind@gmail.com>
2025-03-13 21:20:01 -0400

    Adds Tomb Raider Collection 2 to the website

---
+++ src/pages/games/tr2.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import ContentPage from "../../components/contentpage";
+import TR1Content from "../../content/games/tr2.mdx";
+
+const Tr2Page = () => (
+  <ContentPage>
+    <TR2Content />
+  </ContentPage>
+);
+
+export default Tr2Page;
+
+export const Head = () => <title>Tomb Raider Collection 2</title>;
